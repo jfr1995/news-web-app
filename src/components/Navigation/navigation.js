e3877016d7bb5669fd1d5050e4be649e579ab9fe
Juan Ramirez <juanramirez1776@gmail.com>
2019-09-19 22:28:59 -0700

    refactor navigation

--- src/components/Navigation/navigation.js
+++ src/components/Navigation/navigation.js
@@ -4,28 +4,9 @@ import { Link } from "react-router-dom";
 import * as ROUTES from "../../constants /routes";
 import * as ROLES from "../../constants /roles";
 import { AuthUserContext } from "../Session/session";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 33
-  },
-  menuButton: {
-    marginRight: theme.spacing(20)
-  },
-  title: {
-    flexGrow: 44
-  }
-}));
-
-const handleSignIn = () => {
-  console.log("handle sign in invoked");
-};
+import Box from "@material-ui/core/Box";
 
 const Navigation = () => {
   return (
@@ -41,27 +22,27 @@ const Navigation = () => {
   );
 };
 
+const linkStyles = makeStyles({
+  root: {
+    textDecoration: "none",
+    textTransform: "uppercase"
+  }
+});
 const NavigationNonAuth = () => {
-  const classes = useStyles();
+  const test = linkStyles();
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          ></IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit" onClick={handleSignIn}>
-            Sign In
-          </Button>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <Box>
+      <Box display="inline-flex" justifyContent="flex-start">
+        <Link className={test.root} to={ROUTES.LANDING}>
+          Landing
+        </Link>
+      </Box>
+      <Box display="inline-flex" justifyContent="flex-end">
+        <Link className={test.root} to={ROUTES.SIGN_IN}>
+          Sign In
+        </Link>
+      </Box>
+    </Box>
   );
 };
 
---
+++ src/components/Navigation/navigationAuth.js
@@ -0,0 +1,25 @@
+import React from "react";
+// for HOC -withStyles
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Icon from "@material-ui/core/Icon";
+import Toolbar from "@material-ui/core/Toolbar";
+import AppBar from "@material-ui/core/AppBar";
+import Typography from "@material-ui/core/Typography";
+import { relative } from "path";
+
+const styles = ({ breakpoint, palatte }) => ({
+  root: {
+    width: "100%",
+    textAlign: "left",
+    height: 700,
+    position: "relative",
+    color: "#ffffff",
+    [breakpoints.only("xs")]: {
+      position: "relative",
+      height: 566
+    }
+  }
+});
+
+const navigationNonAuth = withStyles(styles)();
