4034816e6a23c59a83edb1bc1eb73cc53de26811
Juan Ramirez <juanramirez1776@gmail.com>
2020-01-08 14:52:20 -0800

    changes to feed and card components

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,11 +1,19 @@
 import React from "react";
-import Card from "./Card";
+import cx from "clsx";
+import { makestyles } from "@material-ui/core/styles/makeStyles";
+import CardMedia from "@material-ui/core/CardMedia";
+import Card from "@material-ui/core/Card";
+import CardContet from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import TextInfoCardContent from "./Card/TextInfoCardContent";
+import { blogCardContentStyles } from "./Card/blogCardContent.styles";
+import overShadowStyles from "./Card/overShadow.styles";
 
 const Feed = ({ articles }) => {
   return (
     <div>
       {articles.map(article => {
-        return <Card key={article.url} article={article} />;
+        return <div />;
       })}
     </div>
   );
