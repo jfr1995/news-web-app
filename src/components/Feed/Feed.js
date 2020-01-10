376b8d8631a14ead5f7622a5fcbfab0a13b218d2
Juan Ramirez <juanramirez1776@gmail.com>
2020-01-09 18:44:03 -0800

    card styling

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,16 +1,5 @@
 import React, { Fragment } from "react";
-import cx from "clsx";
-import { makestyles } from "@material-ui/core/styles/makeStyles";
-import CardMedia from "@material-ui/core/CardMedia";
-import Card from "@material-ui/core/Card";
-import CardContet from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
-import TextInfoCardContent from "./Card/TextInfoCardContent";
-import { blogCardContentStyles } from "./Card/blogCardContent.styles";
-import overShadowStyles from "./Card/overShadow.styles";
-
-//const useStyles = makeStyles();
-
+import BlogCard from "./Card/Card";
 const Feed = ({ articles }) => {
   console.log(articles);
   return (
