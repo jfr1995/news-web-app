d5418cd8670adc44bcceaaa0252bd524143e3736
Juan Ramirez <juanramirez1776@gmail.com>
2020-01-10 14:08:05 -0800

    fixed mui treasury card styling

--- src/components/Feed/Card/Card.js
+++ src/components/Feed/Card/Card.js
@@ -1,13 +1,14 @@
 import React from "react";
 import cx from "clsx";
-import { makeStyles } from "@material-ui/core/styles/makeStyles";
-import CardMedia from "@material-ui/core/CardMedia";
+import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
-import TextInfoCardContent from "./TextInfoCardContent";
-import blogCardContentStyles from "./blogCardContent.styles";
-import overShadowStyles from "./overShadow.styles";
+import TextInfoCardContent from "@mui-treasury/components/cardContent/textInfo";
+import { useBlogCardContentStyles } from "@mui-treasury/styles/cardContent/blog";
+import { useOverShadowStyles } from "@mui-treasury/styles/shadow/over";
+import { createMuiTheme } from "@material-ui/core";
 
 const useStyles = makeStyles(({ breakpoints, spacing }) => ({
   root: {
@@ -71,8 +72,8 @@ const BlogCard = () => {
   const {
     button: buttonStyles,
     ...cardContentStyles
-  } = blogCardContentStyles();
-  const shadowStyles = overShadowStyles();
+  } = useBlogCardContentStyles();
+  const shadowStyles = useOverShadowStyles();
   return (
     <Card className={cx(styles.root, shadowStyles.root)}>
       <CardMedia
--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -2,16 +2,7 @@ import React, { Fragment } from "react";
 import BlogCard from "./Card/Card";
 const Feed = ({ articles }) => {
   console.log(articles);
-  return (
-    <div>
-      {articles.articles.map(item => (
-        <Fragment key={item.url}>
-          <h2>{item.title}</h2>
-          <p>{item.description}</p>
-        </Fragment>
-      ))}
-    </div>
-  );
+  return <BlogCard />;
 };
 
 export default Feed;
